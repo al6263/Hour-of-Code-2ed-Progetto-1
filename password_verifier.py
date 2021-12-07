d96a843e859860d92ff6d9f8dbe2d324413ad162
-Alberto- <alberto.bella@protonmail.com>
2021-12-07 20:01:02 +0100

    Update password_verifier.py

--- password_verifier.py
+++ password_verifier.py
@@ -1,24 +1,26 @@
 import re
 
 while True:
-    
+    password = input("dimmi la password da inserire: ")
+    if len(password) < 8:
+        print("la password deve essere lunga almeno 8 caratteri!")
+        continue
+    if not re.search("[A-Z]+", password): 
+        print("la password deve contenre almeno una lettera maiuscola!")
+        continue
+    if not re.search("[a-z]+", password): 
+        print("la password deve contenre almeno una lettera minuscola!")
+        continue
+    if not re.search("[0-9]+", password): 
+        print("la password deve contenre almeno un numero!")
+        continue
+    if not re.search("[\.,_-]+", password): 
+        print("la password deve contenre almeno un carattere speciale!")
+        continue
 
-    # La password deve essere lunga almeno 8 caratteri
-    
+    print("la password è valida!")
+    break
 
-    # La password deve contenere almeno un carattere maiuscolo
     
-
-    # La password deve contenere almeno un carattere minuscolo
-    
-
-    # La password deve contenere almeno un numero
-    
-
-    # La password deve contenere almeno un carattere speciale: .,_-
-    
-    
-    pass
-
 
 print("Chiudo il programma.")
